0d27ae50c7d959c8e1426a6b6cd7f9712e2fcd06
Pavel.Balozhka <Pavel.Balozhka@itechart-group.com>
2022-06-25 21:55:12 +0300

    feat: use promise api for loading file

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -1,8 +1,8 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 import { WebSocketServer } from 'ws';
 import { WebSocketController } from "./WebSocketController.js";
+import fs from 'node:fs/promises';
 
 const PORT = process.env.PORT || 8080;
 
@@ -11,7 +11,7 @@ const server = new WebSocketServer({ port: PORT });
 export let webSocket = null;
 
 server.on('connection', ws => {
-    console.log('Success Connect !)');
+    console.log('Success Connection!');
 
     webSocket = ws;
 
@@ -22,30 +22,19 @@ server.on('connection', ws => {
     });
 });
 
-
-// robot.setMouseDelay(2);
-//
-// var twoPI = Math.PI * 2.0;
-// var screenSize = robot.getScreenSize();
-// var height = (screenSize.height / 2) - 10;
-// var width = screenSize.width;
-//
-// for (var x = 0; x < width; x++)
-// {
-//     const y = height * Math.sin((twoPI * x) / width) + height;
-//     robot.moveMouse(x, y);
-// }
-
-export const httpServer = http.createServer(function (req, res) {
+export const httpServer = http.createServer(async (req, res) => {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
+
+    try {
+        const readHtmlStream = await fs.readFile(file_path);
+
         res.writeHead(200);
-        res.end(data);
-    });
+        res.end(readHtmlStream);
+    } catch (error) {
+        res.writeHead(404);
+        res.end(JSON.stringify(error));
+
+        throw new Error('Something wrong with loading html file');
+    }
 });
